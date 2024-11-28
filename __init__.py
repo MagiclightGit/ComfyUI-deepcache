63c2712cec9d7dcdfc9b1204cf1c45f54939ca4b
shonenada <shonenada@gmail.com>
2024-11-28 16:24:00 +0800

    feat: add DeepCache v2

--- __init__.py
+++ __init__.py
@@ -3,5 +3,18 @@ try:
 except ImportError:
 	pass
 else:
-	from .DeepCache import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-	__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+	from .DeepCache import DeepCache
+	from .DeepCacheV2 import DeepCacheV2
+
+
+NODE_CLASS_MAPPINGS = {
+	"DeepCache": DeepCache,
+	"DeepCacheV2": DeepCacheV2,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+	"DeepCache": "Deep Cache",
+	"DeepCacheV2": "Deep Cache - V2",
+}
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
