96c9f8a135615672a29323eaa794b2042d6ea712
shonenada <shonenada@gmail.com>
2024-11-28 16:35:55 +0800

    feat: add DeepCacheV2

--- DeepCacheV2.py
+++ DeepCacheV2.py
@@ -9,6 +9,7 @@ cache_depth: depth of caching
 '''
 
 import torch
+from torch import nn
 from comfy.ldm.modules.diffusionmodules.openaimodel import forward_timestep_embed, timestep_embedding, th, apply_control
 
 class DeepCacheV2:
@@ -54,13 +55,18 @@ class DeepCacheV2:
     def apply(self, model, cache_interval, cache_depth, start_step, end_step):
         new_model = model.clone()
 
+        model_step = 0
         current_t = -1
         current_step = -1
         cache_h = None
+        encoder_hid_proj = nn.Linear(4096, 2048, bias=True)
+        encoder_hid_proj = encoder_hid_proj.to('cuda')
 
         def apply_model(model_function, kwargs):
+            pass
 
-            nonlocal current_t, current_step, cache_h
+        def apply_model(model_function, kwargs):
+            nonlocal current_t, current_step, cache_h, model_step
             
             xa = kwargs["input"]
             t = kwargs["timestep"]
@@ -81,6 +87,9 @@ class DeepCacheV2:
             xc = xc.to(dtype)
             t = new_model.model.model_sampling.timestep(t).float()
             context = context.to(dtype)
+            with torch.cuda.amp.autocast(enabled=True):
+                context = encoder_hid_proj(context)
+
             extra_conds = {}
             for o in kwargs:
                 extra = kwargs[o]
@@ -92,31 +101,32 @@ class DeepCacheV2:
             timesteps = t
             y = None if y is None else y.to(dtype)
             transformer_options["original_shape"] = list(x.shape)
-            transformer_options["current_index"] = 0
+            transformer_options["transformer_index"] = 0
             transformer_patches = transformer_options.get("patches", {})
-            """
-            Apply the model to an input batch.
-            :param x: an [N x C x ...] Tensor of inputs.
-            :param timesteps: a 1-D batch of timesteps.
-            :param context: conditioning plugged in via crossattn
-            :param y: an [N] Tensor of labels, if class-conditional.
-            :return: an [N x C x ...] Tensor of outputs.
-            """
+
             unet = new_model.model.diffusion_model
+            num_video_frames = kwargs.get("num_video_frames",
+                                          unet.default_num_video_frames)
+            image_only_indicator = kwargs.get("image_only_indicator", None)
+            time_context = kwargs.get("time_context", None)
 
+            model_step += 1
 
-            # unet次回実行はtimestepが上がってると仮定・・Refiner等でエラーが起きるかも
             if t[0].item() > current_t:
+                model_step = 0
                 current_step = -1
 
             current_t = t[0].item()
-            apply = 1000 - end_step <= current_t <= 1000 - start_step # tは999->0
+            # apply = 1000 - end_step <= current_t <= 1000 - start_step # tは999->0
+            apply = start_step <= model_step <= end_step
 
             if apply:
                 current_step += 1
             else:
                 current_step = -1
             current_t = t[0].item()
+
+            print(f'model_step: {model_step}, {apply}')
 
             # https://github.com/comfyanonymous/ComfyUI/blob/629e4c552cc30a75d2756cbff8095640af3af163/comfy/ldm/modules/diffusionmodules/openaimodel.py#L598-L659
 
@@ -127,14 +137,30 @@ class DeepCacheV2:
             t_emb = timestep_embedding(timesteps, unet.model_channels, repeat_only=False).to(unet.dtype)
             emb = unet.time_embed(t_emb)
 
+            if "emb_patch" in transformer_patches:
+                patch = transformer_patches["emb_patch"]
+                for p in patch:
+                    emb = p(emb, unet.model_channels, transformer_options)
+
             if unet.num_classes is not None:
                 assert y.shape[0] == x.shape[0]
                 emb = emb + unet.label_emb(y)
 
             h = x.type(unet.dtype)
+
+            step_cache_interval = current_step % cache_interval
+
             for id, module in enumerate(unet.input_blocks):
                 transformer_options["block"] = ("input", id)
-                h = forward_timestep_embed(module, h, emb, context, transformer_options)
+                h = forward_timestep_embed(
+                    module,
+                    h,
+                    emb,
+                    context,
+                    transformer_options,
+                    time_context=time_context,
+                    num_video_frames=num_video_frames,
+                    image_only_indicator=image_only_indicator)
                 h = apply_control(h, control, 'input')
                 if "input_block_patch" in transformer_patches:
                     patch = transformer_patches["input_block_patch"]
@@ -146,23 +172,26 @@ class DeepCacheV2:
                     patch = transformer_patches["input_block_patch_after_skip"]
                     for p in patch:
                         h = p(h, transformer_options)
-                
-                if id == cache_depth and apply: 
-                    if not current_step % cache_interval == 0:
+
+                # input cache
+                if id == cache_depth and apply:
+                    if not step_cache_interval == 0:
                         break # cache位置以降はスキップ
 
-            if current_step % cache_interval == 0 or not apply:
+            # middle cache
+            if step_cache_interval == 0 or not apply:
                 transformer_options["block"] = ("middle", 0)
-                h = forward_timestep_embed(unet.middle_block, h, emb, context, transformer_options)
+                if unet.middle_block is not None:
+                    h = forward_timestep_embed(unet.middle_block, h, emb, context, transformer_options, time_context=time_context, num_video_frames=num_video_frames, image_only_indicator=image_only_indicator)
                 h = apply_control(h, control, 'middle')
 
             for id, module in enumerate(unet.output_blocks):
                 if id < len(unet.output_blocks) - cache_depth - 1 and apply:
-                    if not current_step % cache_interval == 0: 
+                    if not step_cache_interval == 0:
                         continue # cache位置以前はスキップ
                 
                 if id == len(unet.output_blocks) - cache_depth -1 and apply:
-                    if current_step % cache_interval == 0:
+                    if step_cache_interval == 0:
                         cache_h = h # cache
                     else:
                         h = cache_h # load cache
@@ -182,7 +211,7 @@ class DeepCacheV2:
                     output_shape = hs[-1].shape
                 else:
                     output_shape = None
-                h = forward_timestep_embed(module, h, emb, context, transformer_options, output_shape)
+                h = forward_timestep_embed(module, h, emb, context, transformer_options, output_shape, time_context=time_context, num_video_frames=num_video_frames, image_only_indicator=image_only_indicator)
 
             h = h.type(x.dtype)
             if unet.predict_codebook_ids:
